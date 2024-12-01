96fdecf091ca5a9a2b1ce67a48a42715b6aac51c
kwanghwi <nimo6289@gmail.com>
2024-12-01 18:25:21 +0900

    sql 수정

--- FW.sql
+++ FW.sql
@@ -15,10 +15,10 @@ CREATE TABLE `Member` (
 CREATE TABLE `Total-Goal` (
     `goal_id` UUID NOT NULL,
     `member_id` UUID NOT NULL,
-    `total_budget` FLOAT NULL DEFAULT 0.0,
-    `total_expense` FLOAT NULL DEFAULT 0.0,
-    `total_over` FLOAT NULL DEFAULT 0.0,
-    `total_remaining` FLOAT NULL DEFAULT 0.0,
+    `total_budget` FLOAT NULL DEFAULT 0.0 CHECK (`total_budget` >= 0),
+    `total_expense` FLOAT NULL DEFAULT 0.0 CHECK (`total_expense` >= 0),
+    `total_over` FLOAT NULL DEFAULT 0.0 COMMENT,
+    `total_remaining` FLOAT NULL DEFAULT 0.0 CHECK (`total_remaining` >= 0) COMMENT 'Derived',
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`goal_id`),
     FOREIGN KEY (`member_id`) REFERENCES `Member`(`member_id`)
@@ -26,7 +26,7 @@ CREATE TABLE `Total-Goal` (
 
 CREATE TABLE `Semi-Goal` (
     `semi_id` UUID NOT NULL,
-    `semi_budget` FLOAT NULL DEFAULT 0.0,
+    `semi_budget` FLOAT NULL DEFAULT 0.0 CHECK (`semi_budget` >= 0),
     `category_id` INT NOT NULL,
     PRIMARY KEY (`semi_id`),
     FOREIGN KEY (`category_id`) REFERENCES `Category`(`category_id`)
@@ -43,9 +43,9 @@ CREATE TABLE `Semi-Goal Process` (
     `semi_process_id` UUID NOT NULL,
     `member_id` UUID NOT NULL,
     `goal_id` UUID NOT NULL,
-    `semi_expense` FLOAT NULL DEFAULT 0.0,
+    `semi_expense` FLOAT NULL DEFAULT 0.0 CHECK (`semi_expense` >= 0),
     `semi_over` FLOAT NULL DEFAULT 0.0,
-    `semi_remaining` FLOAT NULL DEFAULT 0.0,
+    `semi_remaining` FLOAT NULL DEFAULT 0.0 CHECK (`semi_remaining` >= 0),
     `semi_id` UUID NOT NULL,
     PRIMARY KEY (`semi_process_id`),
     FOREIGN KEY (`member_id`) REFERENCES `Member`(`member_id`),
@@ -57,9 +57,9 @@ CREATE TABLE `Surplus` (
     `surplus_id` UUID NOT NULL,
     `goal_id` UUID NOT NULL,
     `member_id` UUID NOT NULL,
-    `surplus_budget` FLOAT NULL DEFAULT 0.0,
-    `surplus_expense` FLOAT NULL DEFAULT 0.0,
-    `surplus_remaining` FLOAT NULL DEFAULT 0.0,
+    `surplus_budget` FLOAT NULL DEFAULT 0.0 CHECK (`surplus_budget` >= 0),
+    `surplus_expense` FLOAT NULL DEFAULT 0.0 CHECK (`surplus_expense` >= 0),
+    `surplus_remaining` FLOAT NULL DEFAULT 0.0 CHECK (`surplus_remaining` >= 0),
     PRIMARY KEY (`surplus_id`),
     FOREIGN KEY (`goal_id`) REFERENCES `Total-Goal`(`goal_id`),
     FOREIGN KEY (`member_id`) REFERENCES `Member`(`member_id`)
@@ -70,7 +70,7 @@ CREATE TABLE `Expense` (
     `semi_id` UUID NOT NULL,
     `goal_id` UUID NOT NULL,
     `member_id` UUID NOT NULL,
-    `price` FLOAT NULL DEFAULT 0.0,
+    `price` FLOAT NULL DEFAULT 0.0 CHECK (`price` >= 0),
     `item` VARCHAR(255) NULL DEFAULT '',
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
